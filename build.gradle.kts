9ffa08bc36bf2fddbd75b22a8ad2005b8c62e5cb
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2026-03-19 17:09:40 +0200

    feat: Configure Firebase integration and update build settings for iOS and Android

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.androidKotlinMultiplatformLibrary) apply false
     alias(libs.plugins.androidLint) apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 tasks.register("exportModuleDeps") {
--- spendooApp/build.gradle.kts
+++ spendooApp/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    id("com.google.gms.google-services")
 }
 
 val localProperties = Properties()
