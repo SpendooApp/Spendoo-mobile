585d380228f13ba7924cffdaa759c91fba24b2cb
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2026-04-21 18:41:20 +0200

    feat: Update feature module to include categories data and domain modules

--- spendooApp/build.gradle.kts
+++ spendooApp/build.gradle.kts
@@ -73,6 +73,8 @@ kotlin {
             implementation(projects.identityApi)
             implementation(projects.homeApi)
             implementation(projects.homePresentation)
+            implementation(projects.categoriesDomain)
+            implementation(projects.categoriesData)
             implementation(projects.categoriesApi)
             implementation(projects.categoriesPresentation)
         }
